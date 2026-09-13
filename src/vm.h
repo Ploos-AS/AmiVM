311237266caa1379a98dd5187d694e4f3f4979e5
pos <pgousdal@users.noreply.github.com>
2026-09-14 00:31:38 +0200

    M1: define explicit device registry contract

--- src/vm.h
+++ src/vm.h
@@ -14,10 +14,18 @@
 #define AMIVM_VMSERIAL_BASE (AMIVM_MMIO_BASE + 0x0000u)
 #define AMIVM_TIMER_BASE    (AMIVM_MMIO_BASE + 0x1000u)
 #define AMIVM_IRQ_BASE      (AMIVM_MMIO_BASE + 0x2000u)
+#define AMIVM_MMIO_PAGE_SIZE 0x1000u
 
 struct amivm_config {
     size_t ram_size;
     const char *rom_path;
+};
+
+struct amivm_device_desc {
+    const char *name;
+    uint32_t base;
+    uint32_t size;
+    unsigned irq_line;
 };
 
 struct amivm_vm {
@@ -34,6 +42,10 @@ bool amivm_parse_size_mib(const char *text, size_t *bytes_out);
 int amivm_vm_init(struct amivm_vm *vm, const struct amivm_config *config);
 void amivm_vm_destroy(struct amivm_vm *vm);
 int amivm_vm_load_rom(struct amivm_vm *vm, const char *path);
+
+size_t amivm_device_count(void);
+const struct amivm_device_desc *amivm_device_at(size_t index);
+const struct amivm_device_desc *amivm_find_device(const char *name);
 
 bool amivm_read8(struct amivm_vm *vm, uint32_t addr, uint8_t *value);
 bool amivm_write8(struct amivm_vm *vm, uint32_t addr, uint8_t value);
