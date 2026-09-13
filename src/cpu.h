4637f2446a5f3c366ddfa7ae4f2d0488d03a7580
pos <pgousdal@users.noreply.github.com>
2026-09-14 00:59:57 +0200

    M2.3: add exception state to CPU contract

--- src/cpu.h
+++ src/cpu.h
@@ -13,15 +13,23 @@ enum amivm_cpu_fault {
     AMIVM_CPU_FAULT_ILLEGAL,
 };
 
+enum amivm_cpu_exception_vector {
+    AMIVM_VECTOR_BUS_ERROR = 2,
+    AMIVM_VECTOR_ADDRESS_ERROR = 3,
+    AMIVM_VECTOR_ILLEGAL_INSTRUCTION = 4,
+};
+
 struct amivm_cpu_state {
     uint32_t d[8];
     uint32_t a[8];
     uint32_t pc;
+    uint32_t vbr;
     uint16_t sr;
     bool stopped;
     enum amivm_cpu_fault last_fault;
     uint32_t fault_address;
     uint16_t fault_opcode;
+    uint8_t last_exception_vector;
 };
 
 struct amivm_cpu_backend {
