165db24d35847d806f533b6dde8d4c69ffbf36db
pos <pgousdal@users.noreply.github.com>
2026-09-14 23:09:56 +0200

    M2.31: fix strict qualification build

--- tests/test_exec_m231.c
+++ tests/test_exec_m231.c
@@ -33,7 +33,6 @@ int main(void)
     const struct amivm_cpu_backend *backend = amivm_cpu_reference_backend();
     const uint32_t initial_sp = AMIVM_RAM_BASE + 0x1000u;
     const uint32_t block_a = AMIVM_ROM_BASE + 0x200u;
-    const uint32_t block_b = AMIVM_ROM_BASE + 0x220u;
 
     amivm_config_init(&config);
     config.ram_size = 1024u * 1024u;
